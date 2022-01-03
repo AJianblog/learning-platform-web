b08a6f9d4a732d5829a67c464706fd9c1700ec38
hzj <1305696249@qq.com>
2022-01-03 20:37:06 +0800

    菜单增删查改service操作

---
+++ src/app/@core/system/service/impl/menu.service.impl.ts
@@ -0,0 +1,58 @@
+import { MenuService } from "../menu.service";
+import { Observable } from "rxjs";
+import { ResponseResult } from "../../../common/entity/ResponseResult";
+import { Menu } from "../../entity/menu";
+import { HttpClient } from "@angular/common/http";
+import { ProxyPrefix } from "../../../common/enum/ProxyPrefix";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuServiceImpl extends MenuService {
+
+  private url: string = `${ProxyPrefix.api}/menu`;
+
+  constructor(private http: HttpClient) {
+    super();
+  }
+  /**
+   * 查询用户的菜单数据
+   */
+  findUserMenu(): Observable<ResponseResult<Menu[]>> {
+    return this.http.get<ResponseResult<Menu[]>>(`${this.url}/findUserMenu`);
+  }
+
+  /**
+   * 通过菜单id查询菜单信息
+   * @param id 菜单id
+   */
+  findMenuById(id: string | undefined): Observable<ResponseResult<Menu>> {
+    return this.http.get<ResponseResult<any>>(`${this.url}/findByMenuId/${id}`);
+  }
+
+  /**
+   * 新增菜单
+   * @param menu 菜单信息
+   */
+  addMenu(menu: Menu): Observable<ResponseResult<Menu>> {
+    return this.http.post<ResponseResult<Menu>>(`${this.url}/addMenu`, menu);
+  }
+
+  /**
+   * 更新菜单信息
+   * @param menu 菜单信息
+   */
+  updateMenu(menu: Menu): Observable<ResponseResult<Menu>> {
+    return this.http.put<ResponseResult<Menu>>(`${this.url}/updateMenu`, menu);
+  }
+
+  /**
+   * 删除菜单信息,通过id
+   * @param id 菜单id
+   */
+  deleteMenuById(id: string | undefined): Observable<ResponseResult<number>> {
+    return this.http.delete<ResponseResult<number>>(`${this.url}/deleteMenuById/${id}`);
+  }
+
+}
