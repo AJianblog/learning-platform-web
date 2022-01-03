63bb97e9166438c0de84e0bf892385dbed6b8f74
何志坚 <1305696249@qq.com>
2022-01-03 20:37:06 +0800

    菜单增删查改service操作

---
+++ src/app/@core/system/service/menu.service.ts
@@ -0,0 +1,36 @@
+import { Observable } from "rxjs";
+import { ResponseResult } from "../../common/entity/ResponseResult";
+import { Menu } from "../entity/menu";
+
+export abstract class MenuService {
+
+  /**
+   * 查询用户的菜单数据
+   */
+  abstract findUserMenu(): Observable<ResponseResult<Menu[]>>;
+
+  /**
+   * 通过菜单id查询菜单信息
+   * @param id 菜单id
+   */
+  abstract findMenuById(id: string | undefined): Observable<ResponseResult<Menu>>;
+
+  /**
+   * 新增菜单
+   * @param menu 菜单信息
+   */
+  abstract addMenu(menu: Menu): Observable<ResponseResult<Menu>>;
+
+  /**
+   * 更新菜单信息
+   * @param menu 菜单信息
+   */
+  abstract updateMenu(menu: Menu): Observable<ResponseResult<Menu>>;
+
+  /**
+   * 删除菜单信息,通过id
+   * @param id 菜单id
+   */
+  abstract deleteMenuById(id: string | undefined): Observable<ResponseResult<number>>;
+
+}
