bee72676a6435c2a351f36c20e25377feafe87ac
hzj <1305696249@qq.com>
2022-01-03 20:36:19 +0800

    coreModule导入和导出

---
+++ src/app/@core/core.module.ts
@@ -0,0 +1,38 @@
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from "@angular/core";
+import { UserService } from "./system/service/user.service";
+import { UserImplService } from "./system/service/impl/user.impl.service";
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { LoadSvgResource } from "../utils/loadSvgResource";
+import { MenuServiceImpl } from "./system/service/impl/menu.service.impl";
+import { MenuService } from "./system/service/menu.service";
+
+
+const SERVICE = [
+  { provide: UserService, useClass: UserImplService },
+  { provide: MenuService, useClass: MenuServiceImpl }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: []
+})
+export class CoreModule {
+
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    if (parentModule) {
+      throw new Error('CoreModule has already been loaded. Import Core modules in the AppModule only.');
+    }
+    LoadSvgResource(matIconRegistry, domSanitizer);
+  }
+
+  static forRoot(): ModuleWithProviders<any> {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        ...SERVICE
+      ]
+    }
+  }
+}
