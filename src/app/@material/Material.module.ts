d312b0342a5570143e9ff77daa6f201b63d688eb
何志坚 <1305696249@qq.com>
2022-01-03 20:36:00 +0800

    materialModule导入和导出

---
+++ src/app/@material/Material.module.ts
@@ -0,0 +1,41 @@
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSelectModule } from "@angular/material/select";
+
+const MAT_MODULE = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatDialogModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatSelectModule
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ...MAT_MODULE
+  ],
+  exports: [
+    ...MAT_MODULE
+  ]
+})
+export class MaterialModule {
+  static forRoot(): ModuleWithProviders<any> {
+    return {
+      ngModule: MaterialModule,
+      providers: []
+    }
+  }
+}
