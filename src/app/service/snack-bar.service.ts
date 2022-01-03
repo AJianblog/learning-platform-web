8ee810bf21d2676597d7a3f5424eeba89ad39373
何志坚 <1305696249@qq.com>
2022-01-03 20:45:48 +0800

    SnackBarService编写

---
+++ src/app/service/snack-bar.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackBarService extends MatSnackBar {
+}
