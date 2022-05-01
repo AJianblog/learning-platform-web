f474ed911a692a136705f76f5fd9de97c5a343e5
hzj <1305696249@qq.com>
2022-05-01 17:47:06 +0800

    将数据设置的code和message去掉，在拦截器中进行判断

--- src/app/interceptor/ErrorHandleInterceptor.ts
+++ src/app/interceptor/ErrorHandleInterceptor.ts
@@ -12,9 +12,10 @@ import {
   HttpStatusCode
 } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { tap } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { NzMessageService } from "ng-zorro-antd/message";
+import { Router } from "@angular/router";
 
 const messageBlackList = ['操作成功']
 
@@ -22,7 +23,8 @@ const messageBlackList = ['操作成功']
 export class ErrorHandleInterceptor implements HttpInterceptor {
 
   private snackBar: MatSnackBar | undefined;
-  constructor(private injector: Injector, private messageService: NzMessageService) {
+
+  constructor(private injector: Injector, private messageService: NzMessageService, private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -45,10 +47,24 @@ export class ErrorHandleInterceptor implements HttpInterceptor {
           }
           if (error.status === HttpStatusCode.Unauthorized) {
             localStorage.clear()
+            this.router.navigate(['/login'])
           }
         }
-      )
+      ),
+      map(event => this.parseResponseData(event))
     );
+  }
+
+  /**
+   * 将数据设置的code和message去掉
+   * @param event
+   */
+  parseResponseData(event: HttpEvent<any>) {
+    if (event instanceof HttpResponse && event.body) {
+      return event.clone({ body: event.body.data });
+    } else {
+      return event
+    }
   }
 
 }
