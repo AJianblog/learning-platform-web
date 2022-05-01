7f71a6b2b6008607aca37cb919389ffbac1173a8
何志坚 <1305696249@qq.com>
2022-05-01 17:42:00 +0800

    标签分类实体和service编写

---
+++ src/app/@core/article/service/article-type.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from "rxjs";
+import { ArticleType } from "../entity/ArticleType";
+import { PageParam } from "../../common/entity/PageParam";
+import { PageResult } from "../../common/entity/PageResult";
+
+export abstract class ArticleTypeService {
+  /**
+   * 添加文章分类
+   * @param articleType 文章分类信息
+   */
+  abstract addArticleType(articleType: ArticleType): Observable<ArticleType>;
+
+  /**
+   * 删除文章分类
+   * @param articleTypeId 文章分类id
+   */
+  abstract deleteArticleType(articleTypeId: string): Observable<number>;
+
+  /**
+   * 分页查询
+   * @param pageParam 查询条件
+   */
+  abstract page(pageParam: PageParam<ArticleType>): Observable<PageResult<ArticleType>>;
+
+  /**
+   * 更新文章分类
+   * @param articleType 文章分类信息
+   */
+  abstract updateArticleType(articleType: ArticleType): Observable<ArticleType>;
+}
