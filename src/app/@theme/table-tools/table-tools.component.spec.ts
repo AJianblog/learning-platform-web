3c29adb55c383c5e25034407f42457d3ef863f78
hzj <1305696249@qq.com>
2022-05-03 21:57:17 +0800

    表格工具栏

---
+++ src/app/@theme/table-tools/table-tools.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableToolsComponent } from './table-tools.component';
+
+describe('TableToolsComponent', () => {
+  let component: TableToolsComponent;
+  let fixture: ComponentFixture<TableToolsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TableToolsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TableToolsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
