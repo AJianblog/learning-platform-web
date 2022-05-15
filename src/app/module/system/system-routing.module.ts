3d987152108dcb4e0e4935a01cd039497c357ea5
hzj <1305696249@qq.com>
2022-05-15 23:40:07 +0800

    文章管理组件进入和路由配置

--- src/app/module/system/system-routing.module.ts
+++ src/app/module/system/system-routing.module.ts
@@ -5,6 +5,9 @@ import { MenuManagerComponent } from "./menu-manager/menu-manager.component";
 import { UserManagerComponent } from "./user-manager/user-manager.component";
 import { ArticleTypeComponent } from "./article-type/article-type.component";
 import { ArticleTagComponent } from "./article-tag/article-tag.component";
+import { ArticleComponent } from "./article/article.component";
+import { AddArticleComponent } from "./article/add-article/add-article.component";
+import { EditArticleComponent } from "./article/edit-article/edit-article.component";
 
 const routes: Routes = [
   {
@@ -14,7 +17,10 @@ const routes: Routes = [
       { path: 'menuManager', component: MenuManagerComponent },
       { path: 'userManager', component: UserManagerComponent },
       { path: 'articleTypeManager', component: ArticleTypeComponent },
-      { path: 'articleTag', component: ArticleTagComponent }
+      { path: 'articleTag', component: ArticleTagComponent },
+      { path: 'article', component: ArticleComponent },
+      { path: 'addArticle', component: AddArticleComponent },
+      { path: 'editArticle/:id', component: EditArticleComponent }
     ]
   }
 ];
