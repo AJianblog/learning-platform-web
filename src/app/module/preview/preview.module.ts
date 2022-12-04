e93c10cb0b38d9ee1ff993a99680a42b60425d80
何志坚 <1305696249@qq.com>
2022-12-04 22:37:10 +0800

    首页编写

---
+++ src/app/module/preview/preview.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PreviewRoutingModule } from './preview-routing.module';
+import { PreviewComponent } from './preview.component';
+import { ThemeModule } from "../../@theme/theme.module";
+import { HomeComponent } from './home/home.component';
+import { ArticleItemComponent } from './home/article-item/article-item.component';
+import { ZorroModule } from "../../@zorro/zorroModule";
+import { ArticleComponent } from './article/article.component';
+import { EditorModule } from "editor";
+import { TocMenuComponent } from './article/toc-menu/toc-menu.component';
+
+
+@NgModule({
+  declarations: [
+    PreviewComponent,
+    HomeComponent,
+    ArticleItemComponent,
+    ArticleComponent,
+    TocMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    PreviewRoutingModule,
+    ThemeModule,
+    ZorroModule,
+    EditorModule
+  ]
+})
+export class PreviewModule { }
