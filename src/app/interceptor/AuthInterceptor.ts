a3d50b473e1c414a7ea34430b0a0679726293248
何志坚 <1305696249@qq.com>
2023-02-06 22:28:13 +0800

    token登陆拦截器

--- src/app/interceptor/AuthInterceptor.ts
+++ src/app/interceptor/AuthInterceptor.ts
@@ -1,13 +1,14 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { getToken } from "../utils/localStorageMessage";
+import { getToken, tokenIsExpire } from "../utils/localStorageMessage";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = getToken();
-    if (token && !req.headers.has('Authorization')) {
+    // token存在并且没有失效
+    if (token && tokenIsExpire() && !req.headers.has('Authorization')) {
       req = req.clone({
         headers: req.headers.set('Authorization', token)
       })
