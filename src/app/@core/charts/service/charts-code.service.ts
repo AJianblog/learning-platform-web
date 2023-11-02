c2e7192cb7d8ef653ce3cbdc83fbd7b3c8b9b1ca
hzj <1305696249@qq.com>
2023-11-02 22:55:01 +0800

    批量添加图表代码接口

--- src/app/@core/charts/service/charts-code.service.ts
+++ src/app/@core/charts/service/charts-code.service.ts
@@ -1,7 +1,13 @@
 import { Observable } from "rxjs";
 import { ChartsCode } from "../entity/ChartsCode";
+import { PageParam } from "../../common/entity/PageParam";
+import { PageResult } from "../../common/entity/PageResult";
 
 export abstract class ChartsCodeService {
 
+  abstract page(param: PageParam<ChartsCode>): Observable<PageResult<ChartsCode>>
+
   abstract getChartsCodeById(id: string): Observable<ChartsCode>;
+
+  abstract batchAddChartsCode(chartCodeList: ChartsCode[]): Observable<any>;
 }
--- src/app/@core/charts/service/impl/charts-code-impl.service.ts
+++ src/app/@core/charts/service/impl/charts-code-impl.service.ts
@@ -4,6 +4,8 @@ import { ProxyPrefix } from "../../../common/enum/ProxyPrefix";
 import { Observable } from "rxjs";
 import { ChartsCode } from "../../entity/ChartsCode";
 import { HttpClient } from "@angular/common/http";
+import { PageParam } from "../../../common/entity/PageParam";
+import { PageResult } from "../../../common/entity/PageResult";
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +21,18 @@ export class ChartsCodeImplService extends ChartsCodeService {
   getChartsCodeById(id: string): Observable<ChartsCode> {
     return this.http.get<ChartsCode>(`${this.url}/getChartsCodeById/${id}`);
   }
+
+  /**
+   * 批量添加图表代码
+   * @param chartCodeList
+   */
+  batchAddChartsCode(chartCodeList: ChartsCode[]): Observable<any> {
+    return this.http.post(`${this.url}/batchAddChartsCode`, chartCodeList);
+  }
+
+  page(param: PageParam<ChartsCode>): Observable<PageResult<ChartsCode>> {
+    return this.http.post<PageResult<ChartsCode>>(`/diagram/chartsCode/page`, param);
+    // return this.http.post<PageResult<ChartsCode>>(`${this.url}/page`, param);
+  }
+
 }
